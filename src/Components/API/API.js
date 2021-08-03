b42de90aabf0612a5fa5232ff37263ca05b18202
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-03 17:42:19 -0600

    Add foundation for API

--- src/Components/API/API.js
+++ src/Components/API/API.js
@@ -1,5 +1,11 @@
 import React from 'react'
+import { Card } from '../Card/Card'
 
-export const API = () => {
+export const API = ({ data }) => {
 
+   return (
+     <section className= "api-board">
+        <Card />
+      </section>   
+   )
 }
