9b0eaf63cdaac01809a3421a8be86250cddaafa0
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-03 17:43:11 -0600

    Add foundation for fetchAPI function

--- src/Utility/ApiCalls.js
+++ src/Utility/ApiCalls.js
@@ -1,4 +1,11 @@
+import { apiMainUrl } from './url'
 
-export const fetchAPI = {
-  
+export const fetchAPI = async() => {
+  try {
+    const response = await fetch(apiMainUrl)
+    const json = await response.json();
+    console.log(json);
+  }catch(error) {
+    console.log("error", error)
+  }
 }
