1a191e2298b93c3c9e49689f60e59ee12ea0c118
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-03 20:02:51 -0600

    Add foundation for App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,16 +1,28 @@
-import React, { useState } from 'react'
-import API from '../API/API'
-import { fetchAPI } from '../../Utility/ApiCalls';
+import React, { useState,  useEffect  } from 'react';
+import { apiMainUrl } from '../../Utility/url'
+import { ApiCard } from '../ApiBoard/ApiCard'
+import { fetchApi } from '../../Utility/apiCalls'
+import './App.css'
+
+// import { fetchAPI } from '../../Utility/apiCalls';
 import './App.css';
 
 export const App = () => {
-  const [data, setDate] = useState
+  const [playingCards, setCards] = useState([])
 
-  return (
-  <main>
-    <h1>Hello World</h1>
-    <API />
-  </main>
-  )
+  useEffect(() => {
+    (async () => {
+      let {deck_id} = await fetchApi(apiMainUrl);
+      let allCards = await fetchApi(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=52`)
+      setCards(allCards.cards)
+    })();
+  }, []);
+      
+
+    return (
+       <main>
+          <ApiCard cards={playingCards} />
+      </main> 
+    )
 }
 
