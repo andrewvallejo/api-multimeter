056de091670bea9130a90c0881762211b61ac4ec
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-03 19:57:34 -0600

    Add simple api fetch call

--- src/Utility/ApiCalls.js
+++ src/Utility/ApiCalls.js
@@ -1,11 +1,6 @@
-import { apiMainUrl } from './url'
 
-export const fetchAPI = async() => {
-  try {
-    const response = await fetch(apiMainUrl)
-    const json = await response.json();
-    return console.log(json);
-  }catch(error) {
-    return console.log("error", error)
-  }
+export const fetchApi = async (url) => {
+  const response = await fetch(url);
+  const json = await response.json();
+  return json;
 }
