f02d2167f5f458008418e0257365d3df47eb053b
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-05 16:02:45 -0600

    Refactor App to use useReducer

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,27 +1,43 @@
-import React, { useState,  useEffect  } from 'react';
+import React, { useEffect, useReducer  } from 'react';
 import { apiMainUrl } from '../../Utility/url'
 import { ApiCard } from '../ApiCard/ApiCard'
 import { fetchApi } from '../../Utility/apiCalls'
 import './App.css'
-
-// import { fetchAPI } from '../../Utility/apiCalls';
 import './App.css';
 
+const initialState = {
+  deck: []
+}
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'ADD_DECK':
+      return {...state, deck: action.deck}
+    default:
+      return state
+  }
+}
+
 export const App = () => {
-  const [playingCards, setCards] = useState([])
+  // const [playingCards, setCards] = useState([])
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
 
   useEffect(() => {
     (async () => {
-      let {deck_id} = await fetchApi(apiMainUrl);
-      let allCards = await fetchApi(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=52`)
-      setCards(allCards.cards)
+      const {deck_id} = await fetchApi(apiMainUrl);
+      const {cards} = await fetchApi(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=52`)
+      const action = {type: 'ADD_DECK', deck: cards}
+      dispatch(action)
     })();
   }, []);
       
 
     return (
        <main>
-          <ApiCard cards={playingCards} />
+       {console.log(state.deck)}
+          <ApiCard cards={state.deck} />
       </main> 
     )
 }
